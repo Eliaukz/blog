aa01c1f535916f63dd51908ced5d8a3983f4cfab
Eliaukz <U202115586@hust.edu.cn>
2024-06-23 12:23:05 +0800

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://eliaukz.github.io',
+    site: 'https://eliaukz.github.io/blog',
     // base: 'blog',
     integrations: [
         mdx(),
